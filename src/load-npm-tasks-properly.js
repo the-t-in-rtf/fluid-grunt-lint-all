f119c04dba8cecc99552a9ad2bad275dfb525c1b
Tony Atkins <tony@raisingthefloor.org>
2018-07-09 09:27:15 +0200

    GPII-3158: Added documentation for `loadNpmTasksProperly`.

--- src/load-npm-tasks-properly.js
+++ src/load-npm-tasks-properly.js
@@ -5,6 +5,27 @@ var gpii = fluid.registerNamespace("gpii");
 var resolve = require("fluid-resolve");
 
 fluid.registerNamespace("gpii.grunt.lintAll");
+
+/**
+ *
+ * This function works around Grunt's inability to resolve node_modules properly.  By default, `grunt.loadNpmTasks` can
+ * only load modules that are either in a `node_modules` subdirectory immediately beneath the location of the
+ * `Gruntfile.js` you are using, or in a `node_module`s directory in a parent directory.  This makes it difficult to use
+ * more than one level of Grunt plugins, as you must ensure that each package's key dependencies are also available as
+ * (dev) dependencies in your own package.
+ *
+ * This function addresses this limitation by adding a `loadNpmTasks` method which resolves the location of an npm
+ * package to a full path, and then uses `grunt.loadTasks` to load any tasks in the package's `tasks` subdirectory.
+ *
+ * Note: This only partially addresses the shortcomings of Grunt's model, in that you can still have unresolvable
+ * conflicts between Grunt plugins that use different versions of the same library.
+ *
+ * There is some talk of addressing both concerns in the future, you can follow the conversation here:
+ * https://github.com/gruntjs/grunt/issues/1523
+ *
+ * @param {Object } grunt - The Grunt instance (i.e. the result of require-ing Grunt).
+ *
+ */
 gpii.grunt.lintAll.fixGruntTaskLoading = function (grunt) {
     grunt.loadNpmTasksProperly = function (name) {
         var resolved = resolve.sync(name, {
